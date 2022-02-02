05fbab2e5d006123c1ac41aa4ab1ca09bc982346
RenatoDiGiacomo <renato_di_giacomo@hotmail.com>
2022-02-02 01:57:10 -0300

    mudanças pequenas

--- pkproduction/src/App.jsx
+++ pkproduction/src/App.jsx
@@ -1,27 +1,47 @@
 import React from "react";
 import "./css/App.css";
-import PkList from './Base/PkList'
-
+import PkList from "./Base/PkList";
 
 function App() {
-  const url = "https://pokeapi.co/api/v2/"
+  const url = "https://pokeapi.co/api/v2/";
   const [menu, setMenu] = React.useState(null);
-  const [select, setSelect] = React.useState(null);
+  const [selected, setSelected] = React.useState(null);
+  const [page, setPage] = React.useState(null);
 
   async function menuList() {
     const resp = await fetch(url);
     const data = await resp.json();
     setMenu(data);
   }
+
+  async function selectItem() {
+    if (selected) {
+      const respo = await fetch(selected);
+      const data = await respo.json();
+      setPage(data);
+    }
+  }
+
   React.useEffect(menuList, []);
 
-
-
-
- 
-  return <div className="App">
-    <PkList menu={menu} click="aqui"/>
-  </div>;
+  React.useEffect(selectItem, [selected]);
+  return (
+    <>
+      <section style={{ display: "flex", justifyContent: "space-between" }}>
+        <div>
+          <ul>
+            {menu &&
+              Object.entries(menu).map((item) => (
+                <li key={item[1]} onClick={() => setSelected(item[1])}>
+                  {item[0]}
+                </li>
+              ))}
+          </ul>
+        </div>
+        <div>{page && page.count}</div>
+      </section>
+    </>
+  );
 }
 
 export default App;
--- pkproduction/src/Base/PkList.jsx
+++ pkproduction/src/Base/PkList.jsx
@@ -1,25 +1,7 @@
 import React from "react";
 
 const PkList = (props) => {
-  if (props.menu) {
-
-
-    return (
-      <ul>
-        {Object.entries(props.menu).map((item) => (
-          <li key={item[1]} onClick={() => console.log(item[1])}>
-            {item[0]}
-          </li>
-        ))}
-      </ul>
-    );
-  } else {
-    return (
-      <>
-        <p>Página não encontrada</p>
-      </>
-    );
-  }
+ 
 };
 
 export default PkList;
