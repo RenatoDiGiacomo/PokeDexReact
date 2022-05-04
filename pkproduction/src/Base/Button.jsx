9f4cbebef701d5c8a87de2e320a09d7cba736490
Renato Di Giacomo <renato_di_giacomo@hotmail.com>
2022-05-04 12:06:27 -0300

    change project struture

--- pkproduction/src/Base/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Pokemon = ({ dados, props }) => {
-  return <>{dados && <div>{dados}</div>}</>;
-};
-
-export default Pokemon;
--- pkproduction/src/Base/PkList.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PkList = (props) => {
- 
-};
-
-export default PkList;
