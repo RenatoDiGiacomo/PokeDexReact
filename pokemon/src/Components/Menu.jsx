7049879dfe82ba835d23c6ca3030cdc6b52dbe86
Renato Di Giacomo <renato_di_giacomo@hotmail.com>
2022-06-01 20:12:00 -0300

    menu wip

--- pokemon/src/Components/Menu.jsx
+++ pokemon/src/Components/Menu.jsx
@@ -1,9 +1,35 @@
 import React from "react";
 
 const Menu = () => {
+    const [active, setActive] = React.useState(false);
+    const [activeList, setActiveList] = React.useState(false);
+
+    const handleActive = () => {
+        setActive(!active);
+        setInterval(() => {
+            setActiveList(!activeList);
+        }, 1000);
+    };
+
     return (
-        <nav id="menu">
-            <p></p>
+        <nav id="menu" className={active ? "active" : ""} onClick={handleActive}>
+            <ul className={activeList ? "active" : ""}>
+                <li>
+                    <p>Item1</p>
+                </li>
+                <li>
+                    <p>Item1</p>
+                </li>
+                <li>
+                    <p>Item1</p>
+                </li>
+                <li>
+                    <p>Item1</p>
+                </li>
+                <li>
+                    <p>Item1</p>
+                </li>
+            </ul>
         </nav>
     );
 };
